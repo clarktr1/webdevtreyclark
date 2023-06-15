7c50ee1e5609f4ec2f6fbda115cbcba40517482a
Trey Clark <itreyclark@gmail.com>
2023-06-15 09:56:21 -0500

    changes path

--- src/index.js
+++ src/index.js
@@ -8,3 +8,10 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+// https://034291995947.signin.aws.amazon.com/console
+// trey
+// 884688aA@.
+
+// access AKIAQP67WKUVWBGDIYVJ
+// secret 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
